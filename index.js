92b23c1036461ec5a07d7df9bbff321ccb9fe546
claudia welch <claudia.welch73@gmail.com>
2021-12-16 08:18:46 +1100

    commit tree aaa26b5de6fbe14ac32f0250925bcdaea12f1173

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+function saturdayFun(string){
+    if (typeof string === 'undefined'){
+        return "This Saturday, I want to roller-skate!";
+    }
+    else{
+    return `This Saturday, I want to ${string}!`;
+    }
+}
+function mondayWork(string){
+    if (typeof string === 'undefined'){
+        return "This Monday, I will go to the office.";
+    }
+    else{
+    return `This Monday, I will ${string}.`;
+    }
+}
+
+function wrapAdjective(string){
+       return function (special){
+        return `You are ${string}${special}${string}!`;
+    }
+}
+
+wrapAdjective("*")("a hard worker");
+
+const Calculator = {
+    add: function(num1, num2){
+        return num1 + num2;
+    },
+    subtract: function (num1, num2){
+        return num1 - num2;
+    },
+    multiply: function (num1, num2){
+        return num1 * num2;
+    },
+    divide: function (num1, num2){
+        return num1 / num2;
+    }
+}
+
+Calculator.add(1,3);
+Calculator.subtract(1,3);
+Calculator.multiply(1,3);
+Calculator.divide(10,5);
+
+const arrayOfTransforms = [
+    function(a){ return a * 2 },
+    function(a){ return a + 1000},
+    function(a){ return a % 7 }
+]
+
+function actionApplyer(a,array){
+    if (array&&a===0){
+        
+        console.log(`start ${a}`);
+        return a;
+    }
+    else{
+      
+       for (let i=0; i<array.length; i++){
+            a = array[i](a);
+        }
+        let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
+        console.log(message);
+        return a;
+        
+        //a = arrayOfTransforms[0](a);
+        //a = arrayOfTransforms[1](a);
+        //a = arrayOfTransforms[2](a);
+    
+    
+    
+    }    
+}
+actionApplyer(13,arrayOfTransforms);
